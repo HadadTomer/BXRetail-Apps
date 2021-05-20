05042a13a48931b5e5d40595b397424532c87072
Michael Sanchez <michaelsanchez@pingidentity.com>
2021-05-19 17:02:28 -0700

    Ugly group of UI updates to accomodate P1 version of our use cases. I sohuld have broke out the createRef refactor, but too late. :sadpony:.

--- bxretail/src/components/ModalRegister/ModalRegister.js
+++ bxretail/src/components/ModalRegister/ModalRegister.js
@@ -16,7 +16,8 @@ import {
   Col,
   Popover,
   PopoverHeader,
-  PopoverBody
+  PopoverBody,
+  FormText
 } from 'reactstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faQuestionCircle } from '@fortawesome/free-regular-svg-icons';
@@ -27,13 +28,27 @@ import "./ModalRegister.scss";
 // Data
 import data from './data.json';
 
+// Components
+import PingOneAuthN from '../Integration/PingOneAuthN';
+
+const envVars = window._env_;
+console.log("envVars:", envVars);
+const p1AuthN = new PingOneAuthN(envVars.REACT_APP_AUTHPATH, envVars.REACT_APP_ENVId);
+
 class ModalRegister extends React.Component {
   constructor() {
     super();
     this.state = {
       isOpen: false,
-      isPopoverOpen: false
+      isPopoverOpen: false,
+      email: "",            /* PING INTEGRATION: */
+      firstname: "",        /* PING INTEGRATION: */
+      phone: "",            /* PING INTEGRATION: */
+      password: "",         /* PING INTEGRATION: */
+      password_confirm: "", /* PING INTEGRATION: */
+      login: "",            /* PING INTEGRATION: */
     };
+    this.handleFormInput = this.handleFormInput.bind(this); /* PING INTEGRATION: */
   }
   toggle() {
     this.setState({
@@ -45,6 +60,17 @@ class ModalRegister extends React.Component {
       isPopoverOpen: !this.state.isPopoverOpen
     });
   }
+  /* BEGIN PING INTEGRATION: */
+  handleFormInput(event) {
+    //Update state based on the input's Id and value.
+    let formData = {};
+    formData[event.target.id] = event.target.value;
+    this.setState(formData, () => {
+      console.log("STATE:", this.state);
+    });
+  }
+
+  /* END PING INTEGRATION: */
   render() {
     return (
       <div>
@@ -57,27 +83,30 @@ class ModalRegister extends React.Component {
                 <Col md={6}>
                   <FormGroup>
                     <Label for="email">{data.form.fields.email.label}</Label>
-                    <Input type="email" name="email" id="email" placeholder={data.form.fields.email.placeholder} />
+                    <Input onChange={this.handleFormInput} type="email" name="email" id="email" placeholder={data.form.fields.email.placeholder} />
                   </FormGroup>
                   <FormGroup>
                     <Label for="firstname">{data.form.fields.firstname.label}</Label>
-                    <Input type="text" name="firstname" id="firstname" placeholder={data.form.fields.firstname.placeholder} />
+                    <Input onChange={this.handleFormInput} type="text" name="firstname" id="firstname" placeholder={data.form.fields.firstname.placeholder} />
                   </FormGroup>
                   <FormGroup>
                     <Label for="phone">{data.form.fields.phone.label}</Label>
-                    <Input type="tel" name="phone" id="phone" placeholder={data.form.fields.phone.placeholder} />
-                  </FormGroup>
-                  <FormGroup>
-                    <Label for="password">{data.form.fields.password.label}</Label>
-                    <Input type="password" name="password" id="password" placeholder={data.form.fields.password.placeholder} />
-                  </FormGroup>
-                  <FormGroup>
-                    <Label for="password_confirm">{data.form.fields.password_confirm.label}</Label>
-                    <Input type="password" name="password_confirm" id="password_confirm" placeholder={data.form.fields.password_confirm.placeholder} />
+                    <Input onChange={this.handleFormInput} type="tel" name="phone" id="phone" placeholder={data.form.fields.phone.placeholder} />
+                    <FormText>{data.form.fields.phone.requirements}</FormText>
                   </FormGroup>
                 </Col>
                 <Col md={6}>
                   <FormGroup>
+                    <Label for="password">{data.form.fields.password.label}</Label>
+                    <Input onChange={this.handleFormInput} type="password" name="password" id="password" placeholder={data.form.fields.password.placeholder} />
+                  </FormGroup>
+                  <FormGroup>
+                    <Label for="password_confirm">{data.form.fields.password_confirm.label}</Label>
+                    <Input onChange={this.handleFormInput} type="password" name="password_confirm" id="password_confirm" placeholder={data.form.fields.password_confirm.placeholder} />
+                    <FormText>{data.form.fields.password_requirements.text}</FormText>
+                  </FormGroup>
+                  {/* CAN'T DO TRUE OPT-IN PASSWORDLESS IN P1 YET. SEE PROFILE MGMT TO OPT-IN FOR PASSWORD + MFA OPT-IN. */}
+                  {/* <FormGroup> 
                     <Label for="login">
                       {data.form.fields.login.label}
                       <Button id="Popover1" color="link" type="button">
@@ -90,20 +119,20 @@ class ModalRegister extends React.Component {
                     </Label>
                   </FormGroup>
                   <FormGroup>
-                    <Input type="select" name="login" id="login">
+                    <Input onChange={this.handleFormInput} type="select" name="login" id="login">
                       <option>{data.form.fields.login.options.default}</option>
                       <option value="mobile">{data.form.fields.login.options.mobile}</option>
                       <option value="password">{data.form.fields.login.options.password}</option>
                     </Input>
                   </FormGroup>
-                  {/* 
+                  
                   <img src={window._env_.PUBLIC_URL + "/images/google-recaptcha.png"} alt="Google reCAPTCHA" className="google-recaptcha" /> 
                   Removed since we don't offer reCAPTCHA - didn't want to mislead in the demo.
-                  */} 
+                   
                   <FormGroup className="form-group-light ml-3 mb-4">
                     <CustomInput type="checkbox" id="rewards" label={data.form.fields.rewards.label} />
-                  </FormGroup>
-                  <div className="text-right">
+                  </FormGroup>*/}
+                  <div className="text-center">
                     <Button type="button" color="primary" onClick={this.props.onSubmit}>{data.form.buttons.submit}</Button>
                     <Button type="button" color="link" className="ml-3" onClick={this.toggle.bind(this)}>{data.form.buttons.cancel}</Button>
                   </div>
