5216c66de7c6a6f794bd117bd04fd455da1896be
Michael Sanchez <michaelsanchez@pingidentity.com>
2021-07-09 11:22:58 -0700

    Fixed issue with welcome bar and title prop from data file.

--- bxretail/src/pages/any-tv-partner.js
+++ bxretail/src/pages/any-tv-partner.js
@@ -19,6 +19,7 @@ import { faLinkedinIn, faFacebookF, faTwitter, faInstagram } from '@fortawesome/
 
 // Components
 import WelcomeBar from '../components/WelcomeBar/';
+import Session from '../components/Utils/Session'; /* PING INTEGRATION: */
 
 // Data
 import data from '../data/any-tv-partner.json';
@@ -36,7 +37,8 @@ class AnyTVPartner extends React.Component {
     };
     this.showStep2 = this.showStep2.bind(this);
     this.changeAppointment = this.changeAppointment.bind(this);
-    this.envVars = window._env_;
+    this.envVars = window._env_; /* PING INTEGRATION: */
+    this.session = new Session(); /* PING INTEGRATION: */
   }
   toggle() {
     this.setState({
