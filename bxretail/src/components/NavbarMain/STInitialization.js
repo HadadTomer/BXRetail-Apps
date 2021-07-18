1e20fed8817c633b7310d62813058e65fb30c6b6
Tomer Hadad <tomer@securedtouch.com>
2021-07-18 19:16:51 +0300

    securedTouch - login, logout and sessionId integration

--- bxretail/src/components/NavbarMain/STInitialization.js
+++ bxretail/src/components/NavbarMain/STInitialization.js
@@ -1,5 +1,17 @@
-export const asyncInitSecuredTouch = (userId, sessionId) => {
+import { uuidv4 } from "../Utils/UUIDv4";
 
+export const securedTouchSessionId = () => {
+    const key = 'stSessionId';
+    let sessionId = localStorage.getItem(key);
+    if (!sessionId) {
+        sessionId = uuidv4();
+        localStorage.setItem(key, sessionId);
+    }
+
+    return sessionId;
+}
+
+export const asyncInitSecuredTouch = (userId) => {
     const onSecuredTouchReady = callback => {
         if (window['_securedTouchReady']) {
             callback();
@@ -13,11 +25,13 @@ export const asyncInitSecuredTouch = (userId, sessionId) => {
             url: 'https://ping-retail.securedtouch.com',
             appId: 'ping-retail',
             appSecret: 'EJBgAz1mFeQFveSDqD6eYf6Dgs5T',
-            // userId: userId,
-            // sessionId: sessionId,
+            sessionId: securedTouchSessionId(),
             isSingleDomain: true,
         }).then(function () {
             console.log("SecuredTouchSDK initialized successfully");
+            if (userId) {
+                window._securedTouch.login(userId);
+            }
         });
     });
 };
